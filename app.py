3c2031fbccb2a76968e1dc81e54ac27bd2a3a3bb
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2020-11-20 08:29:10 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+def point_to_adress(lon, lat):
+    import requests
+    import warnings
+    url = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lon=" + str(lon) + "&lat=" + str(lat)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        r = requests.get(url=url).json()['address']['city']
+        return r
+
+@app.route('/get/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    lat = request.args.get("lat", None)
+    long = requests.args.get("long",None)
+    response = {}
+    if lat and long:
+    	response["city"] = point_to_adress(long, lat)
+
+    # Return the response in json format
+    return jsonify(response)
+    
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
