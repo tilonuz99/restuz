74ec275526a6aef22cdccdb5a0a2983519cab4ce
tilonuz <59096186+tilonuz99@users.noreply.github.com>
2020-12-08 08:14:39 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,21 @@
 from flask import Flask, request, jsonify
+from geopy import Nominatim
 import requests
 import warnings
 app = Flask(__name__)
 def point_to_adress(lon, lat):
-    url = "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lon=" + str(lon) + "&lat=" + str(lat)
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        r = requests.get(url=url).json()['address']
-        return r
+    return Nominatim(user_agent='Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHT').reverse(str(lat) + ', ' +str(lon), language='uz').raw['address']
 
-@app.route('/get/', methods=['GET'])
+@app.route('/get', methods=['GET'])
 def respond():
-    # Retrieve the name from url parameter
     lat = request.args.get("lat", None)
     long = request.args.get("long",None)
-    response = {}
     if lat and long:
-    	response["city"] = point_to_adress(long, lat)
-
-    # Return the response in json format
-    return jsonify(response)
+    	return jsonify(point_to_adress(long, lat))
     
 @app.route('/')
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
